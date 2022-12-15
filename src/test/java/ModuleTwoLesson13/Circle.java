0b288f792aa369fd5800f724418c624321a688cc
Dmitrii I <kotofelix@yandex.ru>
2022-12-15 12:42:22 +0400

    HomeMade1++

--- src/test/java/ModuleTwoLesson13/Circle.java
+++ src/test/java/ModuleTwoLesson13/Circle.java
@@ -1,13 +1,13 @@
 package ModuleTwoLesson13;
 
 public class Circle {
-    public Circle(double x) {
-        System.out.println("Радиус равен: " + x);
-        System.out.println("Площадь круга равна: " + area(x));
-        System.out.println("Длина окружности равна: " + circumference(x));
+    public Circle(double radius) {
+        System.out.println("Радиус равен: " + radius);
+        System.out.println("Площадь круга равна: " + area(radius));
+        System.out.println("Длина окружности равна: " + circumference(radius));
     }
 
- //   static double radius;
+  //     static double radius;
     static final double Pi = 3.14;
 
     static double area(double radius) {
@@ -20,7 +20,7 @@ public class Circle {
 
     public static void main(String[] args) {
         Circle asd = new Circle(21);
-//        Circle bdsm = new Circle(85.44);
+//        Circle dsa = new Circle(85.44);
 
     }
 }

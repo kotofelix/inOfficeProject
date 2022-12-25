b8db3db990572cf760a060cf7c1243e778119656
Dmitrii <kotofelix@yandex.ru>
2022-12-25 17:54:06 +0400

    работает вывод первого дня введённой недели

---
+++ src/test/java/daysOfWeekHomeMade/Day.java
@@ -0,0 +1,14 @@
+package daysOfWeekHomeMade;
+
+public class Day {
+
+    int dayOfYear;
+    int dayOfWeek;
+    public static int dayOfFirstWeek = 3;
+
+
+//    int startDaysOfWeeks = (numWeek *7) +3;
+
+
+
+}

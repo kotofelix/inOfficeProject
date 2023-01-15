de49b991530adc36e31a494241128f7f2f8859b2
Dmitrii <kotofelix@yandex.ru>
2023-01-16 00:30:33 +0400

    анимал абстрактный + в кар.мэйн создал объекты

--- src/test/java/ModuleTwoLesson17/Animal.java
+++ src/test/java/ModuleTwoLesson17/Animal.java
@@ -30,22 +30,11 @@ public abstract class Animal {
     double foodCounter;     //кол-во запасов пищи
     String location;        //название страны обитания
     static int counter = 0; //все животные имеют внутреннюю связь с сородичами, в переменной содержится общее кол-во осыбей данного вида
-
     HealthState health; //состояние здоровья животного типа HealthState. Возможные значения HealthState: HEALTHY, UNHEALTHY
 
-    public Animal() {
-        counter++;
-    }
-
-    public Animal(String name, double foodCounter, String location) {
-        counter++;
-    }
-
-    void makeSound() {
-    }
+    abstract void makeSound();
 
     //выводит текст, как звучит животное
-
     void eat(double eatFood) {
 
         foodCounter = foodCounter - eatFood;
@@ -54,17 +43,14 @@ public abstract class Animal {
     //выводит текст, что животное ест;
     //уменьшает кол-во еды у животного;
     //также выводит сколько запасов осталось
-
     void sleep() {                              //выводит текст, что животное спит
+
         System.out.println("Животное спит");
     }
-
-
 
     public String toString() {
         return "Страна обитания: " + location + "\nСостояние здоровья: " + health + "\nОбщее количество животных: " + counter;
     }
-
 
     // toString - вовзращает в виде строки текущую информацию об особи
     // Формат вывода:
@@ -72,14 +58,23 @@ public abstract class Animal {
     // Состояние здоровья: ...
     // Сколько особей: ...
     //
-
     public Animal(String name, String food, double foodCounter, String location, HealthState health) {
-        counter++;
         this.name = name;
         this.food = food;
         this.foodCounter = foodCounter;
         this.location = location;
+        counter++;
 
+    }
+
+    public Animal() {
+
+        counter++;
+    }
+
+    public Animal(String name, double foodCounter, String location) {
+
+        counter++;
     }
 
 
--- src/test/java/ru/pflb/Main.java
+++ src/test/java/ru/pflb/Main.java
@@ -10,6 +10,7 @@ package ru.pflb;
 
 import ru.pflb.details.Engine;
 import ru.pflb.professions.Driver;
+import ru.pflb.vehicles.Car;
 import ru.pflb.vehicles.Lorry;
 import ru.pflb.vehicles.SportCar;
 import ru.pflb.vehicles.VehicleClass;
@@ -18,16 +19,32 @@ public class Main {
     public static void main(String[] args) {
         Driver driverLorry1 = new Driver(37, "Vasya", 7, 923423);
         Driver driverSportCar1 = new Driver(27, "Nikolay", 4, 923473);
-        Driver driver1 = new Driver(47, "Boris", 27, 923479);
+        Driver driverCar = new Driver(47, "Boris", 27, 923479);
+
         Engine engineLorry1 = new Engine(2000, "UAZ");
-        Lorry l1 = new Lorry("Maz", VehicleClass.LARGE, 23131, driverLorry1, engineLorry1, 23244);
-        SportCar sportCar1 = new SportCar("Kia", VehicleClass.SPORTS_CAR, 1475, driverSportCar1, engineLorry1);
-    //    System.out.println(driver1.toString());
-        System.out.println(engine1.toString());
-        //      System.out.println(l1.toString());
-        //      l1.turnLeft();
-        //      l1.turnRight();
-        //      l1.start();
-        //      l1.stop();
+        Engine engineSportCar = new Engine(2500, "AUDI");
+        Engine engineCar = new Engine(77, "LADA");
+
+        Lorry lorry1 = new Lorry("Maz", VehicleClass.LARGE, 23131, driverLorry1, engineLorry1, 23244);
+        SportCar sportCar1 = new SportCar("Kia", VehicleClass.SPORTS_CAR, 1475, driverSportCar1, engineSportCar);
+        Car car1 = new Car("Granta", VehicleClass.SMALL, 1148, driverCar, engineCar);
+
+        System.out.println(driverCar.toString());
+        System.out.println();
+        System.out.println(engineCar.toString());
+        System.out.println();
+        System.out.println(lorry1.toString());
+        System.out.println();
+        System.out.println(sportCar1.toString());
+        System.out.println();
+        System.out.println(car1.toString());
+        System.out.println();
+        lorry1.turnLeft();
+        System.out.println();
+        sportCar1.turnRight();
+        System.out.println();
+        car1.start();
+        System.out.println();
+        car1.stop();
     }
 }

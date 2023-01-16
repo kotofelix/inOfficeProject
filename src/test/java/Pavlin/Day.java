2d8dea3cdc2b6549e924b022f97097f5c453753e
Dmitrii I <kotofelix@yandex.ru>
2023-01-16 17:33:13 +0400

    дз по датам

---
+++ src/test/java/Pavlin/Day.java
@@ -0,0 +1,20 @@
+package Pavlin;
+
+public class Day {
+
+    String dayOfWeek;
+    int dayOfMonth;
+
+    public Day(int dayOfWeek, int dayOfMonth) {
+        this.dayOfWeek = DaysOfWeek.Week[dayOfWeek - 1];
+        this.dayOfMonth = dayOfMonth;
+
+    }
+
+    public String toString() {
+        String res;
+        res = String.valueOf(dayOfWeek);
+        return res;
+    }
+
+}

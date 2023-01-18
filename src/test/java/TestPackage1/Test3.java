b489f00b869a98a35afdafb99ade741bffadc901
Dmitrii I <kotofelix@yandex.ru>
2023-01-18 16:49:31 +0400

    +test3

---
+++ src/test/java/TestPackage1/Test3.java
@@ -0,0 +1,36 @@
+/*
+расширяющее преобразование от меньшего к большему
+
+ */
+package TestPackage1;
+
+public class Test3 {
+    public static void main(String[] args) {
+ //      boolean a = true;
+ //      a = !a;
+ //      a = a && !a;
+ //      a = a ^ !a;
+
+  //     int i, j;
+  //     x = 42;
+  //     i = 15;
+
+   //     String a, b = "value";
+
+
+        //   int i, j;
+        //   x = 42;
+        //   i = 15;
+
+        //   String a, b = "value";
+        //   System.out.println(b);
+
+        //      byte byteVar = 127;
+        //      short shortVar = 32767;
+        //      int intVar = 2147483647;
+
+
+
+    }
+
+}

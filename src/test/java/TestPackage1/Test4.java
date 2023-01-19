38dd30c0dee7837b447e7d9ec7c8e13177ba9ded
Dmitrii I <kotofelix@yandex.ru>
2023-01-19 09:26:38 +0400

    Date completed + refactoring++

---
+++ src/test/java/TestPackage1/Test4.java
@@ -0,0 +1,11 @@
+package TestPackage1;
+
+public class Test4 {
+    public static void main(String[] args) {
+        System.out.println(args.length);
+        System.out.println("Hello " + args[1]);
+        System.out.println("Hello " + args[0]);
+        System.out.println("Hello " + args[2]);
+
+    }
+}

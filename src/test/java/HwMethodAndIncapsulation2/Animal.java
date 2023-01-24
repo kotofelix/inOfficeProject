26f9a839de2d943de763994bbaaff0e682aba854
Dmitrii <kotofelix@yandex.ru>
2023-01-24 23:45:41 +0400

    +3 задание

--- src/test/java/HwMethodAndIncapsulation2/Animal.java
+++ src/test/java/HwMethodAndIncapsulation2/Animal.java
@@ -1,19 +1,41 @@
 /*
+Задание 3
 Создать класс животное, сделать его родителем класса Cat.
 Создать статический метод “testStaticMethod” который выводит «Это статическое животное».
 Вывести в методе main этот метод тремя способами.
+
+Задание 4
+Создать в классе Животное статическое поле планета = Земля;
+Вывести на экран данное поле.
+Заменить это поле на Луна
+У созданной кошки вывести данное поле.
  */
 package HwMethodAndIncapsulation2;
-import static HwMethodAndIncapsulation2.Cat.testStaticMethod;
+import static HwMethodAndIncapsulation2.Cat.*;
 
 public class Animal {
-
+    static String planet = "Земля";
+    static {
+        System.out.println(planet);
+    }
+    static {
+        planet = "Луна";
+    }
+    public Animal(){}
+    public Animal(String planet) {
+        this.planet = planet;
+    }
+    public String toString() {
+        return planet;
+    }
     public static void main(String[] args) {
         Cat cat1 = new Cat();
         cat1.testStaticMethod();
         HwMethodAndIncapsulation2.Cat.testStaticMethod();
         testStaticMethod();
 
+        Cat cat2 = new Cat(planet);
+        System.out.println(cat2);
     }
 }
 
--- src/test/java/HwMethodAndIncapsulation2/Cat.java
+++ src/test/java/HwMethodAndIncapsulation2/Cat.java
@@ -7,8 +7,13 @@
 package HwMethodAndIncapsulation2;
 
 public class Cat extends Animal {
+
+    public Cat() {
+    }
+    public Cat(String planet) {
+        super(planet);
+    }
     public static void testStaticMethod() {
         System.out.println("Это статическое животное");
-
     }
 }

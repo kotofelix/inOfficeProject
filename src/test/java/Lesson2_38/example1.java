7c86371a2530aebe27fca610a061cf9b97b89530
Dmitrii I <kotofelix@yandex.ru>
2023-01-30 12:06:44 +0400

    +example1

---
+++ src/test/java/Lesson2_38/example1.java
@@ -0,0 +1,30 @@
+/*
+В программу через аргументы командной строки передаются три целых числа: a, b, c.
+Выведите наибольшее из данных чисел, (программа должна вывести ровно одно целое число).
+
+Проверку на наличие аргументов делать не надо.
+
+
+ */
+package Lesson2_38;
+
+public class example1 {
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+
+        int b = Integer.parseInt(args[1]);
+
+        int c = Integer.parseInt(args[2]);
+
+        if (a>b & a > c){
+            System.out.println("max num is: " + a);
+        } else if (b>a & b>c){
+            System.out.println("max num is: " + b);
+        }
+        else {
+            System.out.println("max num is: " + c);
+        }
+
+    }
+
+}
---
+++ src/test/java/TestPackage1/Test6.java
@@ -0,0 +1,29 @@
+package TestPackage1;
+
+public class Test6 {
+
+
+    public static void main(String[] args) {
+
+
+        int x = -1;
+        String result = "";
+        if (x > 0) if (x == 1) result += "1"; else result += "2";
+        result += "3";
+        switch(x) {
+            case 1-3:
+                result += "4";
+            case 3-4:
+                result += "5";
+            case 4-7:
+                result += "6";
+                break;
+            default:
+                result += "7";
+        }
+        System.out.println(result);
+
+
+        }
+    }
+

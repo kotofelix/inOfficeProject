b15ab40a3e40e1c2397ab350c1c004362cd2d3f1
Dmitrii I <kotofelix@yandex.ru>
2023-02-09 13:20:13 +0400

    lesson 2.42 +task1, +task2

---
+++ src/test/java/Lesson2_42/Task1.java
@@ -0,0 +1,21 @@
+/*
+Напишите программу, которая получает два целых числа m и n
+через аргументы командной строки и печатает целые числа x:
+     m <= x <= n, которые делятся на 3, но не делятся на 5.
+
+Проверку на наличие аргументов делать не надо.
+ */
+package Lesson2_42;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int m = Integer.parseInt(args[0]);
+        int n = Integer.parseInt(args[1]);
+
+        for (int x = m; m<=x && x <=n; x++) {
+            if (x % 3 == 0 && x % 5 != 0) {
+                System.out.println("x = " + x);
+            }
+        }
+    }
+}
---
+++ src/test/java/Lesson2_42/Task4.java
@@ -0,0 +1,4 @@
+package Lesson2_42;
+
+public class Task4 {
+}

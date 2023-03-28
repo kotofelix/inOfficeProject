d23537cac648ffb5ee507869a5beafe600ff3aee
Dmitrii I <kotofelix@yandex.ru>
2023-03-28 13:49:58 +0400

    ++FruitBase

--- src/test/java/Lesson2_58/com/fruitbase/FruitBase.java
+++ src/test/java/Lesson2_58/com/fruitbase/FruitBase.java
@@ -22,31 +22,30 @@ package Lesson2_58.com.fruitbase;
 import Lesson2_58.com.fruitbase.fruits.Fruit;
 
 public class FruitBase {
-    private FruitCatalogue fruitCatalogue; // Объявляем переменную fruitCatalogue типа FruitCatalogue
+    private FruitCatalogue fruitCatalogue;
 
-    public FruitBase() { // Конструктор класса FruitBase
-        this.fruitCatalogue = new FruitCatalogue(); // Создаем новый объект FruitCatalogue
-        // и присваиваем его переменной fruitCatalogue
+    public FruitBase() {
+        this.fruitCatalogue = new FruitCatalogue();
     }
 
-    public Cargo takeOrder(String order) { // Метод takeOrder, который принимает строку с заказом и возвращает объект Cargo
-        String[] orderList = order.split("\\s+"); // Разбиваем строку на отдельные элементы и сохраняем их в массив orderList
-        Cargo cargo = new Cargo(); // Создаем новый объект Cargo
-        for (String orderItem : orderList) { // Проходим по всем элементам массива orderList
-            Fruit fruit = this.fruitCatalogue.findFruit(orderItem); // Ищем фрукт в каталоге по имени orderItem
-            if (fruit != null) { // Если фрукт найден, то
-                cargo.addFruit(fruit); // добавляем его в объект Cargo
+    public Cargo takeOrder(String... order) {
+        Cargo cargo = new Cargo();
+        for (String orderItem : order) {
+            Fruit fruit = this.fruitCatalogue.findFruit(orderItem);
+            if (fruit != null) {
+                cargo.addFruit(fruit);
             }
         }
-        return cargo; // Возвращаем объект Cargo с добавленными фруктами
+        return cargo;
     }
 
     public static void main(String[] args) {
-        FruitBase fruitBase = new FruitBase(); // Создаем новый объект FruitBase
-        if (args.length > 0) { // Если аргументы были переданы при запуске программы, то
-            String order = String.join(" ", args); // Соединяем аргументы в одну строку с разделителем " "
-            Cargo cargo = fruitBase.takeOrder(order); // Получаем объект Cargo с добавленными фруктами
-            System.out.println(cargo); // Выводим информацию о грузе на экран
+        FruitBase fruitBase = new FruitBase();
+        if (args.length > 0) {
+            Cargo cargo = fruitBase.takeOrder(args);
+            System.out.println(cargo);
+        } else {
+            System.out.println("Не переданы аргументы");
         }
     }
 }
